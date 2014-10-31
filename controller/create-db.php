0a96cfed01f37a09e38ed80e8a9e174502ad9592
Kathlyn Miller <katkit101999@hotmail.com>
2014-10-31 12:22:35 -0700

    finish milestone 1-15

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,30 +1,40 @@
 <?php
 require_once(__DIR__ . "/../model/database.php");
 
-   $connection = new mysqli($host, $username, $password);
+   $connection = new mysqli($host, $username, $password); /*putting in database code*/
 
    if($connection->connect_error) {
 
-      die("Error: " . $connection->connect_error);
+      die("<p>Error: " . $connection->connect_error . "</p>");
 
     }
 
-    $exists = $connection->select_db($database);
+    $exists = $connection->select_db($database); /* exists variable equals connection variable to select_db(database) */
 
    if(!$exists) {
    	 $query = $connection->query("CREATE DATABASE $database");
 
    	 if($query) {
-        echo "successfully created database: " . $database;   
+        echo "<p>successfully created database: " . $database ."</p>";   
    	 }
    
    }
 
     else {
-    	echo "Database already exists.";
+    	echo "<p>Database already exists.</p>"; /*paragraph tags in echo */
     }
 
-    $query = $connection->query("CREATE TABLE posts(") 
-      . "id int(11) NOT NULL AUTO_INCREMENT";
+    $query = $connection->query("CREATE TABLE posts(" /* creating table posts in query variable*/
+      . "id int(11) NOT NULL AUTO_INCREMENT," 
+      . "title varchar(255) NOT NULL," 
+      . "post text NOT NULL,"
+      . "PRIMARY KEY (id))");
+
+    if($query) {
+      echo "Successfully created table: posts"; /* This echo only appears once in your php*/
+    }
+    else {
+      echo "<p>$connection->error</p>";  /*paragraph tags in echo */
+    }
    
-   $connection->close();
+   $connection->close(); /* you dont need the ?> tag at the end*/
