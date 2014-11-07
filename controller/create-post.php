09d353d167d2c0f756dbbc9d9062c54b21605b50
Kathlyn Miller <katkit101999@hotmail.com>
2014-11-07 11:01:36 -0800

    Made a title and post variable in create-post.php

---
+++ controller/create-post.php
@@ -0,0 +1,6 @@
+<?php
+  $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING); /*title variable contains filter_input*/
+  $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);  /*post variable contains filter_input*/
+
+   echo "<p>Title: $title</p>" /*echoing paragraph tag with Title:*/
+    echo "<p>Post: $post</p>" /*echoing paragraph tag with Post:*/
