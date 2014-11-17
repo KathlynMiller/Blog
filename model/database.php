5d2886786aca684d78827d54539a7867499740d7
Kathlyn Miller <katkit101999@hotmail.com>
2014-11-17 13:24:26 -0800

    adding constructor and functions in my database.php file

--- model/database.php
+++ model/database.php
@@ -1,6 +1,16 @@
 <?php
+class database { /* we need functions in our class that store in variables*/
+	private $connection; /* these private variables are only to be access on this file*/
+    private $host;   /* no other files can access this class*/
+    private $username; /* these are member,instant,gobal variables*/
+    private $password; /*a class is made with a new instance of an object that will be created and its name must be used when doing this*/
+    private $database;
 
-$host = "localhost";
-$username = "root";
-$password = "root";
-$database = "blog_db";
+    public function __construct( $host, $username, $passoword, $database) {
+      $this->host = $host;
+      $this->username = $username;
+      $this->password = $password;
+      $this->database = $database;
+    }
+
+}
