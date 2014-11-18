d448cfc0dfec03445275e1f3e91777f85eb935bc
Kathlyn Miller <katkit101999@hotmail.com>
2014-11-18 12:17:12 -0800

    Implementing my openConnection and closeConnection functions

--- model/database.php
+++ model/database.php
@@ -6,11 +6,31 @@ class database { /* we need functions in our class that store in variables*/
     private $password; /*a class is made with a new instance of an object that will be created and its name must be used when doing this*/
     private $database;
 
-    public function __construct( $host, $username, $passoword, $database) {
-      $this->host = $host;
+    public function __construct( $host, $username, $passoword, $database) { /* the constructor is cotaining 4 variable from the database class*/
+      $this->host = $host;         /*they all are storing their own gobal variables*/
       $this->username = $username;
       $this->password = $password;
       $this->database = $database;
     }
 
+    public function openConnection() {  /*this function is for opening our connection */
+
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database); /*copied from create-db.php file and contains a connection with its mysqli with variables from database class*/
+
+        if($this->connection->connect_error) {
+
+           die("<p>Error: " . $this->connection->connect_error . "</p>");
+
+        }
+    }
+    public function closeConnection() { /*this function is for closing our connection*/
+    	if(isset($this->connection)) {       /*isset is checking the variable or it will turn null*/
+    		$this->connection->close();    /*isset is checking $this->connection information*/
+
+    	}
+
+    }
+    public function query($string) {   /*query function contains a string variable*/
+
+    }
 }
