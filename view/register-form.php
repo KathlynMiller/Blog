9dd6df2d3a7e0c7d5abf36ebb842ac9ada6ab3e0
Kathlyn Miller <katkit101999@hotmail.com>
2014-12-02 12:11:32 -0800

    Database Table to Store Registered Users

---
+++ view/register-form.php
@@ -0,0 +1,20 @@
+<form>
+	<div>
+		<label for="email">Email: </label> <!-- label tag for email -->
+		<input type="text" name="email"/>  <!-- input tag for text and email -->
+	</div>
+
+	 <div>
+	 	<label for="usernanme">Username: </label> <!-- label tag for username -->
+		<input type="text" name="username"/> <!-- input tag for text and username -->
+	 </div>
+
+	 <div>
+	 	<label for="password">Password: </label> <!-- label tag for password -->
+		<input type="password" name="password"/> <!-- input tag for password -->
+	 </div>
+
+	 <div>
+	 	<button type="submit">Submit</button> <!-- button tag for submit button -->
+	 </div>
+</form>
